eb573918ef53f9e32c301d694d0cca37185724e7
rexjoseph <josephrex96@gmail.com>
2023-08-12 03:33:37 -0700

    create fscEngineTest.t.sol unit testing file

--- script/DeployFSC.s.sol
+++ script/DeployFSC.s.sol
@@ -5,12 +5,26 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {FidesStableCoin} from "../src/FidesStableCoin.sol";
 import {FSCEngine} from "../src/FSCEngine.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFSC is Script {
+  address[] public tokenAddresses;
+  address[] public priceFeedAddresses;
+
   function run() external returns (FidesStableCoin, FSCEngine) {
-    vm.startBroadcast();
+    HelperConfig config = new HelperConfig();
+
+    (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) = config.activeNetworkConfig();
+
+    tokenAddresses = [weth, wbtc];
+    priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
+    vm.startBroadcast(deployerKey);
     FidesStableCoin fsc = new FidesStableCoin();
-    // FSCEngine engine = new FSCEngine();
+    FSCEngine engine = new FSCEngine(tokenAddresses, priceFeedAddresses, address(fsc));
+    
+    fsc.transferOwnership(address(engine));
     vm.stopBroadcast();
+    return (fsc, engine);
   }
 }
--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -18,10 +18,16 @@ contract HelperConfig is Script {
   uint8 public constant DECIMALS = 8;
   int256 public constant ETH_USD_PRICE = 2000e8;
   int256 public constant BTC_USD_PRICE = 1000e8;
+  uint256 public DEFAULT_ANVIL_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80; 
 
   NetworkConfig public activeNetworkConfig;
 
   constructor() {
+    if (block.chainid == 11155111) {
+      activeNetworkConfig = getSepoliaEthConfig();
+    } else {
+      activeNetworkConfig = getOrCreateAnvilEthConfig();
+    }
   }
 
   function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
@@ -50,5 +56,13 @@ contract HelperConfig is Script {
     );
     ERC20Mock wbtcMock = new ERC20Mock("WBTC", "WBTC", msg.sender, 1000e8);
     vm.stopBroadcast();
+
+    return NetworkConfig({
+      wethUsdPriceFeed: address(ethUsdPriceFeed),
+      wbtcUsdPriceFeed: address(btcUsdPriceFeed),
+      weth: address(wethMock),
+      wbtc: address(wbtcMock),
+      deployerKey: DEFAULT_ANVIL_KEY
+    });
   }
 }
